77347c60ba9a04f65c28b462d94eadde52c8711f
NahyeonKim <parakise13@gmail.com>
2023-10-22 11:59:13 +0900

    add  prefetching logic

--- base-lazy-days/client/src/components/app/Home.tsx
+++ base-lazy-days/client/src/components/app/Home.tsx
@@ -1,10 +1,19 @@
+// @ts-nocheck
 import { Icon, Stack, Text } from '@chakra-ui/react';
 import { ReactElement } from 'react';
 import { GiFlowerPot } from 'react-icons/gi';
 
 import { BackgroundImage } from '../common/BackgroundImage';
+import { usePrefetchTreatments } from '../treatments/hooks/useTreatments';
 
 export function Home(): ReactElement {
+  // 미리 fetch해오기위해 만들어준 prefetchTreatments를 home의 최상단에서 실행.
+  // home에서 실행한 이유는 home컴포넌트는 그닥 동적이지 않아서 리렌더가 별로 없기때문에 이 usePrefetchTreatments가 여러번 실행될 걱정을 하지않아도 된다.
+  // 만약 그것이 걱정된다면 usePrefetchTreatments에서 staleTime과 cacheTime을 관리해줄 옵션을 추가
+  // 마운트할때 한번만 실행하기위해서 useEffect를 실행할 수 없는 이유는 이것이 이미 hook이기 때문에 useEffect안에서 훅을 실행할 수는 없다.
+
+  usePrefetchTreatments();
+
   return (
     <Stack textAlign="center" justify="center" height="84vh">
       <BackgroundImage />
--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useQuery, useQueryClient } from 'react-query';
 
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
@@ -17,4 +17,11 @@ export function useTreatments(): Treatment[] {
   const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
 
   return data;
+}
+export function usePrefetchTreatments(): void {
+  // useQueryClient를 가져와서 prefetchQuery 실행
+  const queryClient = useQueryClient();
+
+  // 여기서 키는 캐시에서 어느 useQuery가 이 데이터를 찾아야하는지 알려주기 때문에 정말 중요하다. 여기서 사용한 키가 어떤 useQuery를 사용할지 알려주는 것.
+  queryClient.prefetchQuery(queryKeys.treatments, getTreatments);
 }
